e2985534962c81d569026286041784c9c991a002
veer patel <veerpatel02007@gmail.com>
2026-09-07 22:31:06 +0530

    add:mail functionality WIP

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,21 @@
 import User from "../models/auth.model.js"
 import OTP from "../models/OTP.js";
+import bcrypt from "bcrypt";
+import { sendOtpEmail } from "../utils/email.js";
+
 
 
 export const registerUser = async (req, res) => {
     console.log("start")
     const { name, email, password } = req.body;
+    let userExists = await User.findOne({ email});
+    if(userExists){
+        return res.status(400).json({error:'User already exists'});
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
     try {
         const user = await User.create({
             name,
@@ -29,4 +40,38 @@ export const registerUser = async (req, res) => {
     } catch (error) {
         console.log(error)
     }
+};
+
+//Login User
+
+exports.loginUser = async(req,res)=>{
+    const {email,password}= res.body;
+
+    const user = await User.findone({email});
+    if(!user){
+        return res.status(400).json({error:'Invalid cradentials, Please Sign Up first'});
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.status(400).json({error:'Invalid cradentials'});
+    }
+
+
+isMatch(!user.isVerified){
+    
+}
+
+
+
+
+
+    res.status(200).json({
+        message:'Login successful',
+        user: {
+            
+        }
+    })
+
 }
